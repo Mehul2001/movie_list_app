a8adcf93e747f374f69992534cc79b778c47e139
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-03-04 19:02:52 -0800

    Implement the data dynamicallyand added a search again option as well

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express();
 const request = require("request");
 const port = 3000;
 
-
+app.get("/", (req, res) => {
+    res.render("search.ejs")
+})
 app.get("/results", (req, res) => {
-    request("http://www.omdbapi.com/?s=iowa&apikey=thewdb", (error, response, body) => {
+    var query = req.query.movie;
+    var url = "http://www.omdbapi.com/?apikey=thewdb&s=" + query;
+    request(url, (error, response, body) => {
         if (!error && response.statusCode === 200) {
             const data = JSON.parse(body);
             res.render("results.ejs", { data })
